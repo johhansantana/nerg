8745c241265a13ad0b5e1dfb90cbe815f083e613
Johhan Santana <johhan.santana@gmail.com>
2017-06-23 14:35:20 -0400

    debugging SSR

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ApolloProvider } from 'react-apollo';
+import { ApolloProvider, getDataFromTree } from 'react-apollo';
 import apolloClient from '!/lib/initApollo';
 import initRedux from '!/lib/initRedux';
 import Home from '!/containers/home';
@@ -7,21 +7,46 @@ import Home from '!/containers/home';
  * Component to show the home container.
  */
 class App extends React.Component {
-  static getInitialProps({ req }) {
-    const isServer = !!req;
-    return { isServer };
+  static async getInitialProps(ctx) {
+    let serverState = {};
+    // Evaluate the composed component's getInitialProps()
+    let composedInitialProps = {};
+    if (App.getInitialProps) {
+      composedInitialProps = await App.getInitialProps(ctx);
+    }
+    if (!process.browser) { // eslint-disable-line
+      const apollo = apolloClient();
+      const redux = initRedux(apollo);
+      // Run all graphql queries
+      const app = (
+        <ApolloProvider client={apollo} store={redux}>
+          <Home {...composedInitialProps} />
+        </ApolloProvider>
+      );
+      await getDataFromTree(app);
+      const state = redux.getState();
+      serverState = {
+        apollo: { // Make sure to only include Apollo's data state
+          data: state.apollo.data
+        }
+      };
+    }
+    return {
+      serverState,
+      ...composedInitialProps
+    };
   }
 
   constructor(props) {
     super(props);
     this.apollo = apolloClient();
-    this.store = initRedux(this.apollo);
+    this.store = initRedux(this.apollo, props.serverState);
   }
 
   render() {
     return (
       <ApolloProvider client={this.apollo} store={this.store}>
-        <Home />
+        <Home {...this.props} />
       </ApolloProvider>
     );
   }
